3b8cf4a2cf2f6c3b40caec83f3e518ff7f8a5841
mloine <kinney.xue>
2023-05-26 17:19:32 +0800

    feat: 补充配置目录

--- sql/init.sql
+++ sql/init.sql
@@ -7,8 +7,38 @@ create database t_user_2;
 create database t_user_4;
 
 
+create table sys_dict
+(
+    dict_key               text           not null,
+    dict_value              text  not null
+)
 
-create table SYS_USER
+
+
+create table SYS_USER_0
+(
+    ID                     bigint            not null
+        primary key,
+    STAFF_NO               character(30)           not null,
+    NAME                   character(50),
+    SEX                    smallint,
+    MOBILE                 character(20),
+    EMAIL                  character(50),
+    REMARK                 character(250),
+    PASSWORD               character(150),
+    IS_ENABLE              smallint     default 1 not null,
+    IS_DELETE              smallint     default 1 not null,
+    CREATE_BY              bigint,
+    UPDATE_BY              bigint,
+    CREATE_BY_NAME         character(50),
+    UPDATE_BY_NAME         character(50),
+    CREATE_TIME            DATE,
+    UPDATE_TIME            DATE
+)
+
+
+
+create table SYS_USER_1
 (
     ID                     bigint            not null
         primary key,
