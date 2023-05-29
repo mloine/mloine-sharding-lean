e91a27f720daa3ac33bb514cbbb5c2f40af8a957
mloine <kinney.xue>
2023-05-29 09:45:45 +0800

    feat: proxy 分库分库功能测试配置

--- sql/dql.sql
+++ sql/dql.sql
@@ -1,2 +1,27 @@
 
 insert into sys_dict values('name','xueyongkang');
+
+
+
+insert into sys_user (staff_no, name, sex, mobile, email, remark, password, is_enable, is_delete, create_by, update_by, create_by_name, update_by_name, create_time, update_time)
+values ('0001','xue001',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0002','xue002',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0003','xue003',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0004','xue004',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0005','xue005',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0006','xue006',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0007','xue007',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0008','xue008',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0009','xue009',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0010','xue010',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0011','xue011',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0012','xue012',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0013','xue013',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0014','xue014',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0015','xue015',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0016','xue016',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0017','xue017',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0018','xue018',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0019','xue019',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+     ,('0020','xue020',1,'122342','21312@132.com','','sdada',1,1,1,1,'admin','admin',now(),now())
+;
