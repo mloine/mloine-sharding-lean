1c0caf24322b5e76e6ab86e2217dad995316293c
mloine <kinney.xue>
2023-05-30 09:05:18 +0800

    feat: push

--- command.sh
+++ command.sh
@@ -1,3 +1,7 @@
 psql -h 127.0.01 -p 3307 -d sharding_db -U root
 
-scp config/v1/config-sharding.yaml root@sharding001:/usr/local/shardingsphere/apache-shardingsphere-5.3.2-shardingsphere-proxy-bin/conf
+scp config/v2/config-sharding.yaml root@sharding001:/usr/local/shardingsphere/apache-shardingsphere-5.3.2-shardingsphere-proxy-bin/conf
+
+
+scp config/v1/config-sharding.yaml root@kinneysharding001:/usr/local/shardingsphere/apache-shardingsphere-5.3.2-shardingsphere-proxy-bin/conf
+scp config/v1/SERVER.yaml root@kinneysharding001:/usr/local/shardingsphere/apache-shardingsphere-5.3.2-shardingsphere-proxy-bin/conf
--- sql/init.sql
+++ sql/init.sql
@@ -63,3 +63,7 @@ create table SYS_USER_1
 
 
 
+
+-- Execute SQL Script through logical-database connection , eg: sharding_db. Docker samples configuration in the 'conf/config-sharding.yaml' file
+-- CREATE TABLE t_order ("order_id" serial4, "user_id" int4 NOT NULL, PRIMARY KEY ("order_id"));
+-- CREATE TABLE t_order_item ("order_item_id" serial4, "order_id" int4 NOT NULL, "user_id" int4 NOT NULL, "status" varchar(50), PRIMARY KEY ("order_item_id"));
